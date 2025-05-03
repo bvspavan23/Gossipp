0fde9969b9080d5f80548db66f7047dd76041eb5
bvspavan23 <bvspavan234@gmail.com>
2025-05-03 09:50:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: ["http://localhost:5173","https://gossipp.vercel.app"],
+    origin: ["https://gossipp.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
     transports: ["websocket"],
@@ -22,7 +22,7 @@ const io = socketio(server, {
   },
 });
 app.use(cors({
-  origin: ["http://localhost:5173","https://gossipp.vercel.app"],
+  origin: ["https://gossipp.vercel.app"],
   methods: ["GET", "POST"],
   credentials: true
 }));
